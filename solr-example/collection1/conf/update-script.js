ca152b07bb203edee2ee3fa745ca772e10afa67b
Pannapat Chanpaisaeng <pannapat@users.noreply.github.com>
2014-07-24 22:38:36 +0700

    Update update-script.js
    Remove unused code.

--- solr-example/collection1/conf/update-script.js
+++ solr-example/collection1/conf/update-script.js
@@ -1,16 +1,17 @@
 function processAdd(cmd) {
+  
   doc = cmd.solrDoc;  // org.apache.solr.common.SolrInputDocument
-  var d = new Date(parseInt(doc.getFieldValue("lastUpdateTime")));
-  lastUpdateTime = d.toISOString();
+  
+  var d1 = new Date(parseInt(doc.getFieldValue("lastUpdateTime")));
+  lastUpdateTime = d1.toISOString();
   doc.setField("lastUpdateTime",lastUpdateTime);
   logger.info("update-script#processAdd: lastUpdateTime=" + lastUpdateTime);
   
-  
-  firstCreateTime = doc.getFieldValue("firstCreateTime");
-  var d = new Date(parseInt(doc.getFieldValue("firstCreateTime")));
-  firstCreateTime = d.toISOString();
+  var d2 = new Date(parseInt(doc.getFieldValue("firstCreateTime")));
+  firstCreateTime = d2.toISOString();
   doc.setField("firstCreateTime",firstCreateTime);
   logger.info("update-script#processAdd: firstCreateTime=" + firstCreateTime);
+  
 }
 
 function processDelete(cmd) {
